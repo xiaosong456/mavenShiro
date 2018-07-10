7db9b90f41d6217a1bcba001be61ba046e278050
lxs <1643449009@qq.com>
2018-07-10 22:14:28 +0800

    加入分页和Shiro

---
+++ src/main/java/com/lxsoft/shiro/realms/MyRealm.java
@@ -0,0 +1,27 @@
+package com.lxsoft.shiro.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * MyRealm
+ *
+ * @author lxs
+ * @version v1.0.0
+ */
+public class MyRealm  extends AuthorizingRealm {
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+}
